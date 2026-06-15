faedfaf0e9cc5a095e6de850217f87ac7ee17323
Drtom1 <tomogbonna11@gmail.com>
2026-06-15 15:13:07 +0100

    Add variables for API module configuration in Terraform

---
+++ Cloud/terraform/modules/api/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "read_api_arn" {
+  type = string
+}
+
+variable "command_handler_arn" {
+  type = string
+}
