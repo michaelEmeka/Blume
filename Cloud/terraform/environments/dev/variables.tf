53608836d65d6c8764a8c483a504455af4938a54
Drtom1 <tomogbonna11@gmail.com>
2026-06-15 15:11:35 +0100

    Add Terraform variables for AWS region, environment, and project name

---
+++ Cloud/terraform/environments/dev/variables.tf
@@ -0,0 +1,22 @@
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "project" {
+  description = "Project name"
+  type        = string
+  default     = "blume"
+}
+
+
+
+
