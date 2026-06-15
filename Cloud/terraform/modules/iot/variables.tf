cff1b60caed9522ca1bbdb5992a66cbb9a4a8083
Drtom1 <tomogbonna11@gmail.com>
2026-06-15 15:41:06 +0100

    Add variables for IoT module configuration in Terraform

---
+++ Cloud/terraform/modules/iot/variables.tf
@@ -0,0 +1,6 @@
+
+variable "environment"          { type = string }
+variable "project"              { type = string }
+variable "region"               { type = string }
+variable "account_id"           { type = string }
+variable "stream_processor_arn" { type = string }
